b7f7360ecd6184e47a28904222d2637ac2844e46
tanuutl <tanuutlit@gmail.com>
2024-08-10 17:16:36 +0530

    updated

--- src/api/settings.js
+++
@@ -1,8 +0,0 @@
-import instance from './config';
-
-const settingsApi = {
-    profile:()=> instance.get(`/profile`),
-    resetPassword: (oldPassword,newPassword) => instance.put(`/reset/password`,{ oldPassword, newPassword }),
-
-}
-export default settingsApi;
